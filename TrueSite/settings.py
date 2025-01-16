89c5f0b1411f556474d43977bf4353ff742115a6
KOUOMO Merveille <merveillekouomo@gmail.com>
2025-01-16 05:35:18 +0100

    mot de passe de l'application

--- TrueSite/settings.py
+++ TrueSite/settings.py
@@ -117,12 +117,12 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'  # Fournisseur SMTP
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'merveillekouomo@gmail.com'  # Remplacez par votre adresse email
-EMAIL_HOST_PASSWORD = 'Amby#gmz'  # Remplacez par votre mot de passe
-DEFAULT_FROM_EMAIL = 'TrueSite Technologie <merveillekouomo@gmail.com>'
+EMAIL_HOST_USER = 'merveillekouomo@gmail.com'  # Votre email Gmail
+EMAIL_HOST_PASSWORD = 'obux uhmi msno cagc'  # Mot de passe d'application
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
 # URL via laquelle les fichiers seront accessibles
